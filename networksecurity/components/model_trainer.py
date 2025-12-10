fb4a9f8cf37a79bc57672073cf1c1c6fc7d04f2f
malla-anant <mallaanant2003@gmail.com>
2025-12-10 13:39:24 +0530

    completed

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -7,8 +7,6 @@ from networksecurity.logging.logger import logging
 from networksecurity.entity.artifact_entity import DataTransformationArtifact,ModelTrainerArtifact
 from networksecurity.entity.config_entity import ModelTrainerConfig
 
-
-
 from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 from networksecurity.utils.main_utils.utils import save_object,load_object
 from networksecurity.utils.main_utils.utils import load_numpy_array_data,evaluate_models
